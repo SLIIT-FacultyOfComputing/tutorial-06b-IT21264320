38b90839da46f8324694c150fae93a905efb3cd1
IT21264320 <99391510+IT21264320@users.noreply.github.com>
2022-03-23 04:23:00 +0000

    completed

--- Box.cpp
+++ Box.cpp
@@ -2,33 +2,25 @@
 
 // Implement setters and getters
 
+//Setters
 void Box::setLength(int l) {
-
 	length = l;
 }
-
 void Box::setWidth(int w) {
-
 	width = w;
 }
-
 void Box::setHeight(int h) {
-
 	height = h;
 }
 
+//Getters
 int Box::getLength(void) {
-
 	return length;
 }
-
 int Box::getWidth(void) {
-
 	return width;
 }
-
 int Box::getHeight(void) {
-
 	return height;
 }
 
